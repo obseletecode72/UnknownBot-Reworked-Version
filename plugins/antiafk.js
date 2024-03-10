1fc1616e8fe6df46a5931d880dca47748d82cb6c
obseletecode72 <138137862+obseletecode72@users.noreply.github.com>
2024-03-10 20:31:42 -0300

    Fixes

--- plugins/antiafk.js
+++ plugins/antiafk.js
@@ -1,16 +1,24 @@
 let antiAfkState = {};
 let antiAfkInterval = {};
+let antiAfkDelay = {};
 
 module.exports = function (bot) {
   if (!global.listadecommandos.includes("$antiafk")) {
     global.listadecommandos.push("$antiafk");
-    global.Syntax += '<span style="color:white">- AntiAFK, Descrição do comando:</span> <span style="color:orange">$antiafk</span><br/>';
+    global.Syntax += '<span style="color:white">- AntiAFK, Pula a cada 5 segundos por default (delay pode ser trocado)</span> <span style="color:orange">$antiafk [delay]</span><br/>';
   }
 
   bot.commandChecks['antiafk'] = function (message) {
     if (message.startsWith('$')) {
       if (global.listadecommandos.some(cmd => message.toLowerCase().startsWith(cmd.toLowerCase()))) {
-        if (message.toLowerCase() === "$antiafk") {
+        if (message.toLowerCase().startsWith("$antiafk")) {
+          let delay = message.split(" ")[1]; // Obtém o delay após o comando $antiafk
+          if (delay) {
+            antiAfkDelay[bot.username] = parseInt(delay);
+          } else {
+            antiAfkDelay[bot.username] = 5000; // Valor padrão se nenhum delay for especificado
+          }
+
           antiAfkState[bot.username] = !antiAfkState[bot.username];
           let cor = antiAfkState[bot.username] ? 'green' : 'red';
           let status = antiAfkState[bot.username] ? 'ativado' : 'desativado';
@@ -20,7 +28,7 @@ module.exports = function (bot) {
             antiAfkInterval[bot.username] = setInterval(() => {
               bot.setControlState('jump', true);
               setTimeout(() => bot.setControlState('jump', false), 100);
-            }, 5000);
+            }, antiAfkDelay[bot.username]);
           } else {
             clearInterval(antiAfkInterval[bot.username]);
           }
