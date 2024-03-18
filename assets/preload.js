9b29b7bd03c1851a82373f9fb1c6cf5bce70ed93
obseletecode72 <138137862+obseletecode72@users.noreply.github.com>
2024-03-17 22:00:22 -0300

    Delete assets directory

--- assets/preload.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const input = document.getElementById('captcha-input');
-  input.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      window.ipcRenderer.send('captcha-input', this.value);
-    }
-  });
-});
