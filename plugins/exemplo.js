8cc75195cbdd2f5dbb4b9afcd496bca12d26f526
obseletecode72 <138137862+obseletecode72@users.noreply.github.com>
2024-03-17 22:00:34 -0300

    Delete plugins directory

--- plugins/exemplo.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(bot) {
-  bot.on('spawn', function() {
-    bot.chat('Olá, mundo!');
-  });
-};
-
-
-// aqui apenas estamos brincando com eventos e nao comandos entao nao ha nescessidade de checkCommands
