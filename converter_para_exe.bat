30832eebea9ea4c8e4f5bda45e004d45540cc7b5
obseletecode72 <138137862+obseletecode72@users.noreply.github.com>
2024-08-26 19:45:42 +0100

    Add files via upload

--- converter_para_exe.bat
+++ converter_para_exe.bat
@@ -2,5 +2,10 @@
 set "CURRENT_DIR=%~dp0"
 cd /d "%CURRENT_DIR%"
 
+echo Instalando dependencias...
 npm install
+echo Instalacao principal finalizada...
+echo Iniciando 2 parte
+powershell -Command "Expand-Archive -Path 'node_modules.zip' -DestinationPath 'node_modules' -Force"
+echo Convertendo...
 node converter-para-exe.js
